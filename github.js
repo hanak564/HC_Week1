96148142c8b902a4bffebbaae55b7c2c9f353fda
Hana Kontrec <hk5gc@gmail.com>
2019-02-13 17:45:30 -0500

    Exercises

---
+++ github.js
@@ -0,0 +1,31 @@
+/*
+Exercise File for Week 1 Source, see Week1.txt for details
+*/
+
+/*
+These are functions, but why the weird syntax?
+*/
+const personOne = favoriteFoods => {
+  console.log("My favorite food is pasta.")
+};
+
+const personTwo = favoriteMovies => {
+  /*insert functionality here*/
+};
+
+const personThree = favoriteAnimals => {
+  /*insert functionality here*/
+};
+
+/*
+What does this variable syntax do?
+*/
+const favoriteFoods = [],
+  favoriteMovies = [],
+  favoriteAnimals = [];
+
+const iceBreaker = () => {
+  /*
+    Call each function here
+    */
+};
